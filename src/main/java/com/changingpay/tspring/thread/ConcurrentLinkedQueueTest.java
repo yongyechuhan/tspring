295eb74ff6004a657114401cdce51210f492ea0f
liuxin <liuxin@Changingpay.com>
2017-01-13 12:16:38 +0800

    提交测试concurrentlinkedqueue的代码。

---
+++ src/main/java/com/changingpay/tspring/thread/ConcurrentLinkedQueueTest.java
@@ -0,0 +1,30 @@
+package com.changingpay.tspring.thread;
+
+import org.springframework.stereotype.Component;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * Created by 公司 on 2017/1/13.
+ */
+@Component
+public class ConcurrentLinkedQueueTest {
+    private static ConcurrentLinkedQueue<Integer> linkedQueue =
+            new ConcurrentLinkedQueue<Integer>();
+
+    public static int count = 2;
+
+    public static void offer(){
+        for(int i = 0 ;i < 10000 ;i++){
+            linkedQueue.add(i);
+        }
+    }
+
+    public static class poll implements Runnable{
+        @Override
+        public void run() {
+            while(!(linkedQueue.size() < 1)){
+                System.out.println(Thread.currentThread().getName()+":"+linkedQueue.poll());
+            }
+        }
+    }
+}
---
+++ src/test/java/com/changingpay/threadtest/ThreadTest.java
@@ -0,0 +1,27 @@
+package com.changingpay.threadtest;
+
+import com.changingpay.tspring.thread.ConcurrentLinkedQueueTest;
+import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * Created by 公司 on 2017/1/13.
+ */
+@ContextConfiguration("classpath:applicationContext.xml")
+public class ThreadTest {
+    @Test
+    public void startThread(){
+        ConcurrentLinkedQueueTest.offer();
+        ConcurrentLinkedQueueTest.poll p = new ConcurrentLinkedQueueTest.poll();
+        for(int i = 0; i < ConcurrentLinkedQueueTest.count; i++){
+            Thread t = new Thread(p);
+            t.setName(String.valueOf(i));
+            t.start();
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
