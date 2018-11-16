3925c933b6ccb6bb9142e8290999606d3e8f6ea5
liuxi <liuxinaihui@live.com>
2018-11-16 14:15:50 +0800

    添加jedis集群功能测试。

---
+++ src/main/java/com/changingpay/tspring/business/jedis/JedisFunction.java
@@ -0,0 +1,27 @@
+package com.changingpay.tspring.business.jedis;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import redis.clients.jedis.JedisCluster;
+
+@Service
+public class JedisFunction {
+
+    private Logger logger = LoggerFactory.getLogger(JedisFunction.class);
+
+    @Autowired
+    private JedisCluster jedisCluster;
+
+    public void getVal(){
+        for(int i = 0; i < 100; i++){
+            try {
+                Thread.sleep(1000);
+                logger.info("获取缓存值{},当前请求次数{}", jedisCluster.get("abcuserId"), i);
+            } catch (Exception e) {
+                logger.error("获取值失败", e);
+            }
+        }
+    }
+}
---
+++ src/test/java/com/changingpay/jedistest/TestJedisFunction.java
@@ -0,0 +1,21 @@
+package com.changingpay.jedistest;
+
+import com.changingpay.tspring.business.jedis.JedisFunction;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:applicationContext.xml",
+        "classpath*:spring-jedis.xml"})
+public class TestJedisFunction {
+    @Autowired
+    private JedisFunction jedisFunction;
+
+    @Test
+    public void testExecFunction(){
+        jedisFunction.getVal();
+    }
+}
